5050029b97c78bd3a1f83b4d34b3daf224324dce
RuneScaping <runescapop@proton.me>
2023-09-10 07:36:09 +0800

    Improved example

--- day4/func.asm
+++ day4/func.asm
@@ -27,3 +27,34 @@ io_stihlt:              ; void io_stihlt(void);
   RET
 
 io_in8:                 ; int io_in8(int port);
+  MOV     EDX, [ESP+4]  ; port
+  MOV     EAX, 0
+  IN      AL, DX
+  RET
+
+io_in16:                ; int io_in16(int port);
+  MOV     EDX, [ESP+4]  ; port
+  MOV     EAX, 0
+  IN      AX, DX
+  RET
+
+io_in32:                ; int io_in32(int port);
+  MOV     EDX, [ESP+4]  ; port
+  IN      EAX, DX
+  RET
+
+io_out8:                ; void io_out8(int port, int data);
+  MOV     EDX, [ESP+4]  ; port
+  MOV     AL, [ESP+8]   ; data
+  OUT     DX, AL
+  RET
+
+io_out16:               ; void io_out16(int port, int data);
+  MOV     EDX, [ESP+4]  ; port
+  MOV     AX, [ESP+8]   ; data
+  OUT     DX, AX
+  RET
+
+io_out32:               ; void io_out32(int port, int data);
+  MOV     EDX, [ESP+4]  ; port
+  MOV     EAX, [ESP+8]  ; da
