0f298ebd253e7d9eeb193a68b11dcff16bdf8b26
RuneScaping <runescapop@proton.me>
2023-10-14 21:57:21 +0800

    Update quick-start.md

--- day6/graphic.c
+++ day6/graphic.c
@@ -32,4 +32,28 @@ void set_palette(int start, int end, unsigned char *rgb) {
   io_out8(0x03c8, start);
   for (int i = start; i <= end; i++) {
     io_out8(0x03c9, rgb[0] / 4);
-    io
+    io_out8(0x03c9, rgb[1] / 4);
+    io_out8(0x03c9, rgb[2] / 4);
+    rgb += 3;
+  }
+
+  io_store_eflags(eflags);
+}
+
+void box_fill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0,
+               int x1, int y1) {
+  for (int y = y0; y <= y1; y++) {
+    for (int x = x0; x <= x1; x++) {
+      vram[y * xsize + x] = c;
+    }
+  }
+}
+
+void init_screen8(unsigned char *vram, int x, int y) {
+  box_fill8(vram, x, COL8_840084, 0, 0, x - 1, y - 29);
+  box_fill8(vram, x, COL8_C6C6C6, 0, y - 28, x - 1, y - 28);
+  box_fill8(vram, x, COL8_FFFFFF, 0, y - 27, x - 1, y - 27);
+  box_fill8(vram, x, COL8_C6C6C6, 0, y - 26, x - 1, y - 1);
+
+  box_fill8(vram, x, COL8_FFFFFF, 3, y - 24, 59, y - 24);
+  box_fill8(vram, x, COL8_FFFFFF, 2, y - 24, 2,
