7c38d8cd6e1a6cfcf73ef555ff9bc5691ad33406
RuneScaping <runescapop@proton.me>
2024-06-10 05:41:49 +0800

    whitespace tweak

--- day6/inthandler.asm
+++ day6/inthandler.asm
@@ -7,4 +7,47 @@
 asm_int_handler21:
   PUSH    ES
   PUSH    DS
-  PUSHA
+  PUSHAD
+  MOV     EAX, ESP
+  PUSH    EAX
+  MOV     AX, SS
+  MOV     DS, AX
+  MOV     ES, AX
+  CALL    int_handler21
+  POP     EAX
+  POPAD
+  POP     DS
+  POP     ES
+  IRETD
+
+asm_int_handler27:
+  PUSH    ES
+  PUSH    DS
+  PUSHAD
+  MOV     EAX, ESP
+  PUSH    EAX
+  MOV     AX, SS
+  MOV     DS, AX
+  MOV     ES, AX
+  CALL    int_handler27
+  POP     EAX
+  POPAD
+  POP     DS
+  POP     ES
+  IRETD
+
+asm_int_handler2c:
+  PUSH    ES
+  PUSH    DS
+  PUSHAD
+  MOV     EAX, ESP
+  PUSH    EAX
+  MOV     AX, SS
+  MOV     DS, AX
+  MOV     ES, AX
+  CALL    int_handler2c
+  POP     EAX
+  POPAD
+  POP     DS
+  POP     ES
+  IRETD
---
+++ day6/io.asm
@@ -0,0 +1,7 @@
+  [BITS 32]
+
+  ; 程序中包含函数名
+  GLOBAL io_hlt, io_cli, io_sti, io_stihlt
+  GLOBAL io_in8, io_in16, io_in32
+  GLOBAL io_out8, io_out16, io_out32
+  GLOBAL io_load
