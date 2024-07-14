14e7c42e62c2643242284329a2a136daf03917ed
RuneScaping <runescapop@proton.me>
2024-07-14 15:34:51 +0800

    debuG

--- day7/graphic.c
+++ day7/graphic.c
@@ -129,3 +129,33 @@ void init_mouse_cursor8(char *mouse, char bc) {
       "*OOO*..*OOO*....",
       "*OO*....*OOO*...",
       "*O*......*OOO*..",
+      "**........*OOO*.",
+      "*..........*OOO*",
+      "............*OO*",
+      ".............***"};
+
+  for (int y = 0; y < 16; y++) {
+    for (int x = 0; x < 16; x++) {
+      if (cursor[y][x] == '*') {
+        mouse[y * 16 + x] = COL8_000000;
+      }
+
+      if (cursor[y][x] == 'O') {
+        mouse[y * 16 + x] = COL8_FFFFFF;
+      }
+
+      if (cursor[y][x] == '.') {
+        mouse[y * 16 + x] = bc;
+      }
+    }
+  }
+}
+
+void put_block8_8(unsigned char *vram, int vxsize, int pxsize, int pysize,
+                  int px0, int py0, char *buf, int bxsize) {
+  for (int y = 0; y < pysize; y++) {
+    for (int x = 0; x < pxsize; x++) {
+      vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
+    }
+  }
+}
