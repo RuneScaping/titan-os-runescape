ab1a3aa2826a08ec5bfe49c2cc322ab600922a68
RuneScaping <runescapop@proton.me>
2024-08-11 18:53:06 +0800

    EHCI: Update qTD next pointer in QH overlay region during unlink

--- day7/include/graphic.h
+++ day7/include/graphic.h
@@ -14,4 +14,17 @@
 #define COL8_008400   10
 #define COL8_848400   11
 #define COL8_000084   12
-#define COL8_840084 
+#define COL8_840084   13
+#define COL8_008484   14
+#define COL8_848484   15
+
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
+void box_fill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+void init_screen8(unsigned char *vram, int x, int y);
+void put_font8(unsigned char *vram, int xsize, int x, int y, char c, char *font);
+void put_fonts8_asc(unsigned char *vram, int xsize, int x, int y, char c, char *s);
+void init_mouse_cursor8(char *mouse, char bc);
+void put_block8_8(unsigned char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize);
+
+#endif // _GRAPHIC_H_
---
+++ day7/include/int.h
@@ -0,0 +1,6 @@
+#ifndef _INT_H_
+#define _INT_H_
+
+#define PIC0_ICW1 0x0020
+#define PIC0_OCW2 0x0020
+#defin
