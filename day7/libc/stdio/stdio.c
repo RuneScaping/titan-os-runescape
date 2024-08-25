dc9d4beb74613634a9f8c369c772848d0f887c65
RuneScaping <runescapop@proton.me>
2024-08-25 10:19:12 +0800

    hwmon: (ads1015) Fix out-of-bounds array access

--- day7/libc/stdio/stdio.c
+++ day7/libc/stdio/stdio.c
@@ -23,3 +23,41 @@ void printnum(void (*fputch)(char, void *), void *data, unsigned long num,
     while (--attr.width > attr.negative) {
       fputch(attr.pad, data);
     }
+
+    if (attr.negative) {
+      fputch('-', data);
+    }
+  }
+
+  if (attr.uppercase) {
+    fputch("0123456789ABCDEF"[num % attr.base], data);
+  } else {
+    fputch("0123456789abcdef"[num % attr.base], data);
+  }
+}
+
+void vprintfmt(void (*fputch)(char, void *), void *data, const char *fmt,
+               va_list ap) {
+  int ch;
+  unsigned long num;
+  char *str;
+  struct PrintNumAttr attr;
+
+  while (1) {
+    while ((ch = *fmt++) != '%') {
+      fputch(ch, data);
+      if (ch == '\0') {
+        return;
+      }
+    }
+
+    num = 0;
+    attr.base = 0;
+    attr.pad = ' ';
+    attr.width = 0;
+    attr.negative = 0;
+    attr.uppercase = 0;
+  reswitch:
+    switch (ch = *fmt++) {
+    case '0':
+      attr.pad
